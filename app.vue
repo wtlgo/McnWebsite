9ee31d91eb9f8c1fa7af2ebe75bae2ddcaa04588
wtlgo <wtlgo>
2024-12-16 00:48:51 +0300

    Initial commit

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-app>
+        <v-container>
+            <v-row>
+                <v-spacer />
+                <v-col cols="auto">
+                    <h1>Nothing here yet</h1>
+                </v-col>
+                <v-spacer />
+            </v-row>
+        </v-container>
+    </v-app>
+</template>
+
+<script setup lang="ts">
+useHead({
+    title: "Mikchan no Sekai",
+});
+</script>
