94793a5f13d6ff7dbb90024d765acdb86c1c1ce6
wtlgo <wtlgo>
2024-12-31 16:13:22 +0300

    Add premium data to the list

--- server/utils/get-vk-users.ts
+++ server/utils/get-vk-users.ts
@@ -1,17 +1,34 @@
+import { isNull } from "drizzle-orm";
+
 export const getVkUsers = async () => {
     const db = useMysqlDb();
 
-    const { mcnpVkBukkitPlayers, mcnpVkVkUsers } = mysqlTables;
+    const { mcnpVkBukkitPlayers, mcnpVkVkUsers, premium } = mysqlTables;
 
     return db
         .select({
             name: mcnpVkBukkitPlayers.name,
             uuid: mcnpVkBukkitPlayers.uuid,
             vkId: mcnpVkVkUsers.vkId,
+            premium: premium.premium,
+            floodgate: premium.floodgate,
         })
         .from(mcnpVkBukkitPlayers)
         .innerJoin(
             mcnpVkVkUsers,
             eq(mcnpVkBukkitPlayers.vkUserId, mcnpVkVkUsers.id)
+        )
+        .leftJoin(
+            premium,
+            or(
+                eq(
+                    sql`REPLACE(${mcnpVkBukkitPlayers.uuid}, '-', '')`,
+                    premium.uuid
+                ),
+                and(
+                    or(isNull(mcnpVkBukkitPlayers.uuid), isNull(premium.uuid)),
+                    eq(mcnpVkBukkitPlayers.name, premium.name)
+                )
+            )
         );
 };
