17c35422a8138a6091a3b15b226b5582e763a9dd
wtlgo <wtlgo>
2025-01-07 15:56:53 +0300

    Add auth info

---
+++ components/AuthInfo.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-card
+        v-if="auth.valid"
+        :prepend-avatar="auth.photo"
+        :title="`Добро пожаловать, ${auth.name}!`"
+        :subtitle="auth.isAdmin ? 'Администратор' : 'Игрок'"
+    >
+        <template #actions>
+            <v-btn color="red" @click="onExit">Выйти</v-btn>
+        </template>
+    </v-card>
+</template>
+
+<script lang="ts" setup>
+const { auth, token } = useAuthData();
+const onExit = () => (token.value = null);
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,6 @@
     <v-container>
         <top-cover />
         <v-row>
-            <vk-one-tap />
-        </v-row>
-        <v-row>
             <v-col cols="12" md="12" lg="4">
                 <server-stat ip="play.mikchan.net" />
             </v-col>
@@ -15,7 +12,20 @@
                 <server-stat ip="creative.mikchan.net" />
             </v-col>
         </v-row>
+
+        <v-row v-if="auth.valid">
+            <v-spacer />
+            <v-col cols="auto"> <auth-info /> </v-col>
+            <v-spacer />
+        </v-row>
+        <v-row v-else>
+            <v-spacer />
+            <v-col cols="auto"> <vk-one-tap /> </v-col>
+            <v-spacer />
+        </v-row>
     </v-container>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const { auth } = useAuthData();
+</script>
