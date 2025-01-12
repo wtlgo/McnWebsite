ec3f40702532fdda4e27cf675f2f2dcf4304cad2
wtlgo <wtlgo>
2025-01-13 02:36:52 +0300

    Add vk based search

--- components/CabinetPlayerList.vue
+++ components/CabinetPlayerList.vue
@@ -3,6 +3,8 @@
         <v-text-field label="Поиск" v-model="search" />
     </one-row>
 
+    <one-row> Найдено: {{ filteredData.length }} </one-row>
+
     <v-row>
         <cabinet-player-list-item
             v-for="item in filteredData"
@@ -17,16 +19,30 @@ import type { PlayerListData } from "~/shared/types/player-list-data";
 
 const search = ref("");
 const searchDebounced = refDebounced(search, 500);
+
 const { data } = defineProps<{ data: PlayerListData[] }>();
+const { cache } = useCacheVkUsers();
+
+const searchedIds = computed(() =>
+    cache.value
+        .filter((u) =>
+            `${u.first_name} ${u.last_name}`
+                .toLowerCase()
+                .includes(searchDebounced.value.toLowerCase())
+        )
+        .map((u) => u.id)
+);
+
 const filteredData = computed(() =>
     data
         .map((d, id) => ({ ...d, id }))
         .filter(
             (d) =>
+                d.vk.toString().startsWith(searchDebounced.value.trim()) ||
+                searchedIds.value.includes(d.vk) ||
                 d.name
                     .toLowerCase()
-                    .includes(searchDebounced.value.trim().toLowerCase()) ||
-                d.vk.toString().startsWith(searchDebounced.value.trim())
+                    .includes(searchDebounced.value.trim().toLowerCase())
         )
 );
 </script>
