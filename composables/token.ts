6b4deaf4e28b058f1bb31d0758a6e8b0888586d3
wtlgo <wtlgo>
2025-01-23 10:15:06 +0300

    Extend session time to 1 year

--- composables/token.ts
+++ composables/token.ts
@@ -2,6 +2,7 @@ export const useToken = () => {
     const token = useCookie<string | null>("auth-data", {
         default: () => null,
         watch: true,
+        maxAge: 60 * 60 * 24 * 365,
     });
 
     return token;
